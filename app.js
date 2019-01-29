3eafab5f4cc7c2784d697667248ab08f8422fcac
xueke1106 <xiaoxk1106@163.com>
2019-01-29 18:55:24 +0800

    'test'

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-const
+const http = require('http');
+const conf = require('./config/defaultConfig');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<body>');
+    res.write('hello http!');
+
+    res.write('</body>');
+    res.end('</html>')
+})
+server.listen(conf.port, conf.hostname, () => {
+  const addr = `http://${conf.hostname}:${conf.port}`
+  console.info(`server start at ${addr}`);
+
+});
